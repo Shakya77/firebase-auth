fc133c9d7360d1823c9f602b0b86b1f570c81a5b
Shakya77 <bijanshakya145@gmail.com>
2025-02-20 11:37:53 +0545

    changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { auth } from './Firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import './login.css'
 
 export default function login() {
     const [email, setEmail] = useState("");
@@ -17,19 +18,62 @@ export default function login() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <h3>Login</h3>
-            <div className="">
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            </div>
-            <div className="">
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <div className="">
-                <button type='submit'>Login</button>
-            </div>
-        </form>
+        <div className='center'>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="flex-column">
+                    <label >Email</label>
+                </div>
+                <div className="inputForm">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 32 32" height="20">
+                        <g data-name="Layer 3" id="Layer_3">
+                            <path d="m30.853 13.87a15 15 0 0 0 -29.729 4.082 15.1 15.1 0 0 0 12.876 12.918 15.6 15.6 0 0 0 2.016.13 14.85 14.85 0 0 0 7.715-2.145 1 1 0 1 0 -1.031-1.711 13.007 13.007 0 1 1 5.458-6.529 2.149 2.149 0 0 1 -4.158-.759v-10.856a1 1 0 0 0 -2 0v1.726a8 8 0 1 0 .2 10.325 4.135 4.135 0 0 0 7.83.274 15.2 15.2 0 0 0 .823-7.455zm-14.853 8.13a6 6 0 1 1 6-6 6.006 6.006 0 0 1 -6 6z"></path>
+                        </g>
+                    </svg>
+                    <input
+                        type="email"
+                        className="input"
+                        name="email"
+                        placeholder="Enter your Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+
+                {/* Password Input */}
+                <div className="flex-column">
+                    <label htmlFor="password">Password</label>
+                </div>
+
+                <div className="inputForm">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="-64 0 512 512" height="20">
+                        <path d="m336 512h-288c-26.453125 0-48-21.523438-48-48v-224c0-26.476562 21.546875-48 48-48h288c26.453125 0 48 21.523438 48 48v224c0 26.476562-21.546875 48-48 48zm-288-288c-8.8125 0-16 7.167969-16 16v224c0 8.832031 7.1875 16 16 16h288c8.8125 0 16-7.167969 16-16v-224c0-8.832031-7.1875-16-16-16zm0 0"></path>
+                        <path d="m304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0"></path>
+                    </svg>
+                    <input
+                        type="password"
+                        className="input"
+                        name="password"
+                        placeholder="Enter your Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
+                {/* Remember Me and Forgot Password */}
+                <div className="flex-row">
+                    <div>
+                        <input type="hidden" id="rememberMe" />
+                        <label htmlFor="rememberMe"></label>
+                    </div>
+                    <span className="span">Forgot password?</span>
+                </div>
+
+                {/* Submit Button */}
+                <button type="submit" className="button-submit">Sign In</button>
+
+                {/* Signup Link */}
+                <p className="p">Don't have an account? <span className="span">Sign Up</span></p>
+            </form>
+        </div>
     )
 }
